9c09ab134b43c8e082187ddbcfc9b1f93115ceae
Samuel Ameh <propersam2012@gmail.com>
2019-08-16 10:00:31 +0100

    update chromedriver install script

--- travis/unittest-script.sh
+++ travis/unittest-script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://chromedriver.storage.googleapis.com/763809.68/chromedriver_linux64.zip && \
-    sudo unzip chromedriver_linux64.zip -d /usr/local/bin/
+LATEST_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE) && \
+    wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_linux64.zip && \
+    sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/; || echo 'Error: Failed while trying to install chromedriver'; exit 1
 
 python3 -m pip install selenium --user || sudo python3 -m pip install selenium --user || exit 1 
 
