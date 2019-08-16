230100f06012228c619ee461acc07903b3c2ecb1
Samuel Ameh <propersam2012@gmail.com>
2019-08-16 11:56:40 +0100

    fix calling chmod without sudo

--- travis/unittest-script.sh
+++ travis/unittest-script.sh
@@ -3,7 +3,7 @@
 LATEST_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE) && \
     wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_linux64.zip && \
     sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/ && \
-    chmod 777 /usr/local/bin/chromedriver;
+    sudo chmod 777 /usr/local/bin/chromedriver;
 
 python3 -m pip install selenium --user || sudo python3 -m pip install selenium --user || exit 1 
 
