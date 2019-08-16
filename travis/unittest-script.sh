b5c7879d4ddba55cc91c7bc84d74835dd2f50bac
Samuel Ameh <propersam2012@gmail.com>
2019-08-16 05:11:07 +0100

    update unittest script

--- travis/unittest-script.sh
+++ travis/unittest-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec -it django-defectdojo_uwsgi_1 /bin/bash
+python3 -m pip install selenium
 
-python tests/Product_type_unit_test.py
+python3 tests/Product_type_unit_test.py
 
 exec python tests/Product_unit_test.py
