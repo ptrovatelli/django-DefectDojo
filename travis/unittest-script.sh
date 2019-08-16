f34bc57d6910f54591c477c80f73d990f0a0cf1d
Samuel Ameh <propersam2012@gmail.com>
2019-08-16 09:26:16 +0100

    add chromedriver install to script

--- travis/unittest-script.sh
+++ travis/unittest-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+wget https://chromedriver.storage.googleapis.com/763809.68/chromedriver_linux64.zip && \
+    sudo unzip chromedriver_linux64.zip -d /usr/local/bin/
+
 python3 -m pip install selenium --user || sudo python3 -m pip install selenium --user || exit 1 
 
 python3 tests/Product_type_unit_test.py || echo 'Error: Product type unittest failed.'; exit 1
