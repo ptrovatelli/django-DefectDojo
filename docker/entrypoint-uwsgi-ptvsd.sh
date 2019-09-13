68300e786cd7bc82c0312127d2a78468863ddf1d
Pascal Trovatelli <ptrovatelli@gmail.com>
2019-09-14 00:21:25 +0200

    fix ptvsd

--- docker/entrypoint-uwsgi-ptvsd.sh
+++ docker/entrypoint-uwsgi-ptvsd.sh
@@ -20,4 +20,5 @@ exec uwsgi \
   "--${DD_UWSGI_MODE}" "${DD_UWSGI_ENDPOINT}" \
   --protocol uwsgi \
   --wsgi dojo.wsgi:application \
-  --py-autoreload 1
+  --py-autoreload 1 \
+  --enable-threads --lazy-apps --honour-stdin
