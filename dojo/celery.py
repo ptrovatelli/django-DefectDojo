28c646eed52d59444e4a646a48fffedc81f5f6e6
Pascal Trovatelli <ptrovatelli@gmail.com>
2019-09-14 09:09:56 +0200

    try to configure ptvsd for celery worker

--- dojo/celery.py
+++ dojo/celery.py
@@ -2,6 +2,8 @@
 import os
 from celery import Celery
 from django.conf import settings
+import socket
+from socket import error as socket_error
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dojo.settings.settings')
@@ -13,6 +15,28 @@ app = Celery('dojo')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+print('coucou celery')
+def _check_ptvsd_port_not_in_use(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('127.0.0.1', port))
+    except socket_error as se:
+        return False
+
+    return True
+
+
+ptvsd_port = 3001
+if os.environ.get("DD_DEBUG") == "on" and _check_ptvsd_port_not_in_use(ptvsd_port):
+    try:
+        # enable remote debugging
+        import ptvsd
+        ptvsd.enable_attach(address=('0.0.0.0', ptvsd_port))
+        print("ptvsd listening on port " + ptvsd_port)
+    except Exception as e:
+        print("Generic exception caught with DD_DEBUG on. Passing.")
+
 
 
 @app.task(bind=True)
