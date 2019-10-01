6e3408989fbff60a669343d936e20ae650b221f4
Samuel Ameh <propersam2012@gmail.com>
2019-10-01 14:36:08 +0100

    add -f install to travis

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo apt-get -y update
+sudo apt-get -f install
 sudo apt-get -y install socat curl python3-software-properties
 sudo apt-get install python3-pip python3-setuptools python3-wheel
 sudo -H pip3 install -U setuptools pip
